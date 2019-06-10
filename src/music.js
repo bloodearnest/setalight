5f62c8dc1181cd3b66f255bda7342acecf9be99d
Simon Davy <simon.davy@canonical.com>
2019-06-10 21:36:25 +0000

    fix #/b issue in tansposing

--- src/music.js
+++ src/music.js
@@ -1,13 +1,16 @@
 const NOTES_SHARP = 'A A# B C C# D D# E F F# G G#'.split(' ')
 const NOTES_FLAT = 'A Bb B C Db D Eb E F Gb G Ab'.split(' ')
 const NOTES_ALL = 'A A# Bb B C C# Db D D# Eb E F F# Gb G G# Ab'.split(' ')
+const SHARP_KEYS = 'C Am G Em D Bm A F#m E C#m B G#m F# D#m C# A#m'.split(' ')
+const FLAT_KEYS = 'F Dm Bb Gm Eb Cm Ab Fm Db Bbm Gb Ebm Cb Abm'.split(' ')
+
 
 function calculateTranspose (src, dst) {
   if (src === dst) {
     return { amount: 0, notes: [] }
   }
-  const src_notes = src.indexOf('b') === -1 ? NOTES_SHARP : NOTES_FLAT
-  const dst_notes = dst.indexOf('b') === -1 ? NOTES_SHARP : NOTES_FLAT
+  const src_notes = FLAT_KEYS.includes(src) ? NOTES_FLAT : NOTES_SHARP
+  const dst_notes = FLAT_KEYS.includes(dst) ? NOTES_FLAT : NOTES_SHARP
   const src_index = src_notes.indexOf(src)
   const dst_index = dst_notes.indexOf(dst)
   const amount = dst_index - src_index
